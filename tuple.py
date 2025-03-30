311eabcf1a8eee3b92bdc30d5752eeac9d55df8c
rithwik777222 <rithwikk312@gmail.com>
2025-03-30 21:05:01 +0530

    first commit

---
+++ tuple.py
@@ -0,0 +1,19 @@
+x = ()
+print(x)
+
+x = (1,2,3)
+print(x)
+
+x = (1,"hello",3.4)
+print(x)
+
+x = ("wow",[1,2,3],[4,5,6])
+print(x)
+
+x =('p','q','r','s','t','u')
+print(x[5])
+print(x[0])
+
+x = ("wow",[1,2,3],[4,5,6])
+print(x[0][3])
+print(x[1][1])
